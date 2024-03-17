5559c9be67574e480a2389cfe09bb81825cc4d29
Marcin Słowiak <m.slowiak@smartrecruiters.com>
2024-03-17 21:43:08 +0100

    Simple application module test

--- src/test/java/com/github/mslowiak/demo/notification/NotificationModuleTests.java
+++ src/test/java/com/github/mslowiak/demo/notification/NotificationModuleTests.java
@@ -1,14 +1,38 @@
 package com.github.mslowiak.demo.notification;
 
+import java.util.List;
+import java.util.UUID;
+
+import com.github.mslowiak.demo.order.OrderCreatedEvent;
 import org.junit.jupiter.api.Test;
 import org.springframework.modulith.test.ApplicationModuleTest;
+import org.springframework.modulith.test.Scenario;
 
-@ApplicationModuleTest
+@ApplicationModuleTest //  Bootstrap Modes
 class NotificationModuleTests {
 
     @Test
-    void whatsup() {
+    void bootstrapsNotificationModule() {
 
+    }
+
+    @Test
+    void onceReceivingOrderCreatedEventTheNotificationIsSent(Scenario scenario) {
+        // given
+        final var orderCreatedEvent = new OrderCreatedEvent(
+                UUID.randomUUID(),
+                UUID.randomUUID(),
+                List.of("Apple", "Pillow")
+
+        );
+
+        // when & then
+        scenario.publish(orderCreatedEvent)
+                .andWaitForEventOfType(NotificationSentEvent.class)
+                .matchingMappedValue(NotificationSentEvent::recipientId, orderCreatedEvent.customerId())
+                .matchingMappedValue(NotificationSentEvent::notificationType, "ORDER_CREATED")
+                .matchingMappedValue(NotificationSentEvent::email, "test@gmail.com")
+                .toArrive();
     }
 
 }
